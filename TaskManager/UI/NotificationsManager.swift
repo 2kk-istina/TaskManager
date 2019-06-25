ea71f0160f98a9cfd084f928a1071a486e4dd975
2kk-istina <2kk.istina@gmail.com>
2019-06-25 17:00:59 +0200

    add LocalNotifications

--- TaskManager/UI/NotificationsManager.swift
+++ TaskManager/UI/NotificationsManager.swift
@@ -11,51 +11,28 @@ import UserNotifications
 import UIKit
 
 class NotificationsManager {
-//    static let sharedInstance = NotificationsManager()
-//    var myTask: EntityTask?
+    static let sharedInstance = NotificationsManager()
+    var myTask: EntityTask?
 //    let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
-//    weak var delegate = UIApplication.shared.delegate as? AppDelegate
-//    //
-//    func newNotification(date: Date, uuid: String, body: String) {
-//        delegate?.scheduleNotification(
-//            atDate: date,
-//            title: "You have something to do, remember?",
-//            body: body,
-//            uuid: uuid)
-//    }
-//    //
-//    func unscheduleAllNotifications() {
-//        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
-//    }
-//    //
-//    func scheduleAllNotifications() {
-////        guard let date = myTask?.dateComplete! else {
-////            fatalError("Error")
-////        }
-//        let date = Date().addingTimeInterval(10)
-//        newNotification(date: date,
-//                        uuid: myTask!.uuid!,
-//                        body: "myTask!.taskTitle!")
-//        }
-////    func rescheduleAllNotifications() {
-////        //let request = NSFetchRequest<Task>(entityName: "Task")
-////
-////        do {
-////            let searchResults = try context.fetch(request)
-////
-////            for task in searchResults {
-////
-////                if task.notificationEnabled == true {
-////
-////                    //schedule notification
-////                    newNotification(date: task.date, uuid: task.uuid!, body: task.taskDescription!)
-////                }
-////            }
-////
-////        } catch {
-////            print("Error with request: \(error)")
-////        }
-////
-////        //(UIApplication.shared.delegate as! AppDelegate).saveContext()
-////    }
+    weak var delegate = UIApplication.shared.delegate as? AppDelegate
+    //
+    func newNotification(date: Date, uuid: String, body: String) {
+        delegate?.scheduleNotification(
+            atDate: date,
+            title: "You have something to do, remember?",
+            body: body,
+            uuid: uuid)
+    }
+    //
+    func unscheduleAllNotifications() {
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+    }
+    func rescheduleAllNotifications() {
+        if let myTask = myTask {
+            if myTask.notification == true {
+                //schedule notification
+                newNotification(date: myTask.dateComplete! as Date, uuid: myTask.uuid!, body: myTask.taskTitle!)
+            }
+        }
+    }
 }
